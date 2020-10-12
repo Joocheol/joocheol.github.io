28d5ff03579f3a1f92139b280d236f2a071c8bea
Joocheol <joocheol@yonsei.ac.kr>
2020-10-12 09:28:19 +0900

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -19,5 +19,5 @@ class LikeButton extends React.Component {
   }
 }
 
-let domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(<LikeButton />, domContainer);
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
