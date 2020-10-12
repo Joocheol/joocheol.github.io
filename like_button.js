67578e3de69290ab2e3a582b274cea0a5e4457e8
Joocheol <joocheol@yonsei.ac.kr>
2020-10-12 09:31:16 +0900

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const e = React.createElement;
+
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
@@ -11,13 +13,12 @@ class LikeButton extends React.Component {
       return 'You liked this.';
     }
 
-    return (
-      <button onClick={() => this.setState({ liked: true }) }>
-        Like
-      </button>
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
     );
   }
-}
 
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
